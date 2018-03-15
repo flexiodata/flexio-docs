300e6572cc30f895efabbfbfd1647cbaf24b18d8
David Z. Williams <dave@flex.io>
2018-03-15 15:38:55 -0400

    Added basic app.

---
+++ app/src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div id="app" class="fixed absolute--fill flex flex-column overflow-hidden">
+    <nav class="pa3" style="box-shadow: rgba(0, 0, 0, 0.125) 0px 1px 4px;">
+      <div class="f4">Flex.io Structured Documentation</div>
+    </nav>
+    <router-view class="flex-fill" />
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'App'
+  }
+</script>
---
+++ app/src/components/Home.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="flex flex-row w-100">
+    <div class="br b--black-10" style="min-width: 12rem"><div class="pa3">Sidenav</div></div>
+    <div class="flex-fill"><div class="pa3">Main Content</div></div>
+  </div>
+</template>
+
+<script>
+  export default {
+
+  }
+</script>
