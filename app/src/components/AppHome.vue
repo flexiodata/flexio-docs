246735679129f8da71804834aa3bbee7cd2881e8
David Z. Williams <dave@flex.io>
2018-03-16 18:16:54 -0400

    Double-column YAML/JSON view. JSON output is reactive.

--- app/src/components/AppHome.vue
+++ app/src/components/AppHome.vue
@@ -2,7 +2,7 @@
   <div class="flex flex-row w-100">
     <div class="br b--black-10" style="min-width: 12rem"><div class="pa3">Sidenav</div></div>
     <div class="flex-fill relative">
-      <doc-detail :doc-id="doc_id" />
+      <doc-detail :base-path="base_path" :doc-path="doc_path" />
     </div>
   </div>
 </template>
@@ -10,13 +10,16 @@
 <script>
   import DocDetail from './DocDetail'
 
+  const base_path = 'https://raw.githubusercontent.com/flexiodata/flexio-docs/master'
+
   export default {
     components: {
       DocDetail
     },
     data() {
       return {
-        doc_id: ''
+        base_path,
+        doc_path: '/src/task/convert.yml'
       }
     }
   }
--- app/src/components/DocDetail.vue
+++ app/src/components/DocDetail.vue
@@ -1,10 +1,31 @@
 <template>
-  <div class="absolute absolute--fill">
-    <textarea
-      ref="textarea"
-      spellcheck="false"
-      v-model.trim="json_text"
-    ></textarea>
+  <div class="flex flex-column absolute absolute--fill">
+    <div class="absolute top-0 right-0 z-5 flex flex-row justify-end pa2" v-if="showToggle">
+      <button
+        type="button"
+        class="flex flex-row items-center pointer dim bn bg-blue ttu b white pv1 ph3 br2 mr2"
+        @click="mode = 'json'"
+      >
+        <i class="material-icons nl2 mr1" v-if="mode == 'json'">check</i>
+        <span>JSON</span>
+      </button>
+      <button
+        type="button"
+        class="flex flex-row items-center pointer dim bn bg-blue ttu b white pv1 ph3 br2"
+        @click="mode = 'yaml'"
+      >
+        <i class="material-icons nl2 mr1" v-if="mode == 'yaml'">check</i>
+        <span>YAML</span>
+      </button>
+    </div>
+    <div class="flex-fill flex flex-row">
+      <div class="flex-fill" v-if="!_isMounted || !showToggle || (showToggle && mode == 'yaml')">
+        <textarea ref="textarea-yaml"></textarea>
+      </div>
+      <div class="flex-fill" v-if="!_isMounted || !showToggle || (showToggle && mode == 'json')">
+        <textarea ref="textarea-json"></textarea>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -17,50 +38,81 @@
   require('codemirror/mode/yaml/yaml.js')
   require('codemirror/mode/javascript/javascript.js')
 
-  const base_path = 'https://raw.githubusercontent.com/flexiodata/flexio-docs/master'
-
   export default {
     props: {
-      'doc-id': {
+      'base-path': {
+        type: String,
+        default: 'https://raw.githubusercontent.com/flexiodata/flexio-docs/master'
+      },
+      'doc-path': {
         type: String,
         default: ''
+      },
+      'show-toggle': {
+        type: Boolean,
+        default: false
       }
     },
     watch: {
-      docId: {
+      docPath: {
         handler: 'readYaml',
         immediate: true
       },
-      json_text(val, old_val) {
-        this.editor.setValue(val)
+      yaml_text() {
+        this.editor_json.setValue(this.json_text)
+      },
+      mode() {
+        this.editor_json.setValue(this.json_text)
+        this.editor_yaml.setValue(this.yaml_text)
       }
     },
     data() {
       return {
-        json_text: '',
-        editor: null
+        mode: 'yaml', // 'json' or 'yaml'
+        yaml_text: '',
+        editor_json: null,
+        editor_yaml: null
       }
     },
     mounted() {
-      var opts = {
+      this.editor_yaml = CodeMirror.fromTextArea(this.$refs['textarea-yaml'], {
         lineNumbers: true,
-        mode: 'application/json'
-      }
+        mode: 'text/x-yaml'
+      })
 
-      this.editor = CodeMirror.fromTextArea(this.$refs['textarea'], opts)
-      this.editor.setCursor({ line: 1, ch: 1000000 })
+      this.editor_yaml.on('change', (cm) => {
+        this.yaml_text = cm.getValue()
+      })
+
+      this.editor_json = CodeMirror.fromTextArea(this.$refs['textarea-json'], {
+        lineNumbers: true,
+        mode: 'application/json',
+        readOnly: true
+      })
+    },
+    computed: {
+      json_text() {
+        try {
+          var json_obj = yaml.load(this.yaml_text)
+          return JSON.stringify(json_obj, null, 2)
+        }
+        catch(e) {
+          return JSON.stringify(e, null, 2)
+        }
+      }
     },
     methods: {
       readYaml() {
         try {
-          axios.get(base_path+'/src/task/convert.yml')
+          axios.get(this.basePath+this.docPath)
             .then(response => {
-              var json = yaml.load(response.data)
-              this.json_text = JSON.stringify(json, null, 2)
+              this.yaml_text = response.data
+              this.editor_yaml.setValue(this.yaml_text)
             })
         }
         catch(e) {
-          this.json_text = JSON.stringify(e)
+          this.yaml_text = ''
+          this.json_text = ''
         }
       }
     }
@@ -68,6 +120,10 @@
 </script>
 
 <style>
+  button {
+    font-family: "Open Sans", "Helvetica Neue", Helvetica, Arial, sans-serif;
+  }
+
   .CodeMirror {
     width: 100%;
     height: 100%;
