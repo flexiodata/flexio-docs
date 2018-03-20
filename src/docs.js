8be1218d7d0182a9f241ee4edd8db7fb8cc267fc
David Z. Williams <dave@flex.io>
2018-03-20 19:39:12 -0400

    Fixed problem where assigning result of a loader to a variable was causing an error.

--- src/docs.js
+++ src/docs.js
@@ -1,24 +1,14 @@
 import paths from './paths'
-import { toMarkdown } from './render'
 
 var docs = []
 
 paths.forEach(function(path) {
-  var json_obj = require('json-loader!yaml-loader!../def/' + path + '.yml')
-  var markdown_obj = {}
-
-  for (var key in json_obj) {
-    if (json_obj.hasOwnProperty(key)) {
-      markdown_obj[key] = render.toMarkdown(json_obj[key])
-    }
-  }
-
   try {
     docs.push({
       path: path,
       label: path,
       yaml_text: require('raw-loader!../def/' + path + '.yml'),
-      markdown_obj: markdown_obj,
+      json_obj: require('json-loader!yaml-loader!../def/' + path + '.yml'),
       is_valid: true
     })
   }
@@ -28,7 +18,7 @@ paths.forEach(function(path) {
       path: path,
       label: path,
       yaml_text: '',
-      markdown_obj: {},
+      json_obj: {},
       is_valid: false
     })
   }
--- src/main.js
+++ src/main.js
@@ -1,10 +1,11 @@
 import docs from './docs'
+import render from './render'
 
 var main = {
   docs,
   getMarkdown(path, key) {
     var doc = docs.find(function(doc) { return doc.path === path })
-    return doc && doc.markdown_obj[key] ? doc.markdown_obj[key] : ''
+    return doc && doc.json_obj[key] ? render.toMarkdown(doc.json_obj[key]) : ''
   }
 }
 
