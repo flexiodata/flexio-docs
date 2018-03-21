6a1c85ed8791ea542d2405aa067f06b4e0a54a9c
David Z. Williams <dave@flex.io>
2018-03-20 21:03:37 -0400

    Updated main.js to use `fs` to read local YAML files.

--- src/docs.js
+++ src/docs.js
@@ -5,20 +5,16 @@ var docs = []
 paths.forEach(function(path) {
   try {
     docs.push({
-      path: path,
       label: path,
       yaml_text: require('raw-loader!../def/' + path + '.yml'),
-      json_obj: require('json-loader!yaml-loader!../def/' + path + '.yml'),
       is_valid: true
     })
   }
   catch(e)
   {
     docs.push({
-      path: path,
       label: path,
       yaml_text: '',
-      json_obj: {},
       is_valid: false
     })
   }
--- src/main.js
+++ src/main.js
@@ -1,11 +1,22 @@
+const fs = require('fs')
+const path = require('path')
+const yaml = require('js-yaml')
+
 const docs = require('./docs')
 const render = require('./render')
 
 const main = {
-  docs,
-  getMarkdown(path, key) {
-    var doc = docs.find(function(doc) { return doc.path === path })
-    return doc && doc.json_obj[key] ? render.toMarkdown(doc.json_obj[key]) : ''
+  docs: docs,
+  getMarkdown: function(relative_path, key) {
+    try {
+      var full_path = path.resolve(__dirname, '../def/' + relative_path + '.yml')
+      var doc = yaml.safeLoad(fs.readFileSync(full_path, 'utf8'))
+      return doc && doc[key] ? render.toMarkdown(doc[key]) : ''
+    }
+    catch(e)
+    {
+      return e
+    }
   }
 }
 
